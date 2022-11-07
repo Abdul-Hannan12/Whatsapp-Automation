21324d9d06019011800346be35009f6bfb56ba2e
Abdul Hannan <abdulhannan8540680@gmail.com>
2022-11-07 20:26:30 +0500

    initial commit

---
+++ whatsapp_automation.py
@@ -0,0 +1,8 @@
+import pywhatkit as pwk
+
+# +91 99668 26454 SHAZA
+# +91 78934 50233 SABIHA
+
+# pwk.sendwhatmsg('+92 332 8540680', 'hello', 13, 53)
+# pwk.sendwhatmsg('+91 99668 26454', 'h-9:57 :: m-10:27', 21, 57)
+pwk.sendwhatmsg('+91 78934 50233', 'Happy Birthday Sabiha 💞', 23, 30)
