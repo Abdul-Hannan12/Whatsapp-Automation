dab53d378ada5667b00641df7afbee75fec8b685
Abdul Hannan <abdulhannan8540680@gmail.com>
2022-11-26 18:26:57 +0500

    added spam msg

---
+++ spam_message.py
@@ -0,0 +1,18 @@
+import pyautogui
+import webbrowser as web
+import time
+
+msg = input('enter message to send: ')
+times = int(input('enter the number of times to send the message'))
+
+# win_chrome_path = 'C:\Program Files\Google\Chrome\Application\chrome.exe %s'
+# web.get(win_chrome_path).open('web.whatsapp.com')
+
+web.open('web.whatsapp.com')
+
+time.sleep(30)
+
+for i in range(times):
+    for char in msg:
+        pyautogui.press('space' if char==' ' else char)
+    pyautogui.press('enter')
